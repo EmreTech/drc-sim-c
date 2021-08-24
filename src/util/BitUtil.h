3a6d55bac7407db5c486913132a5b521137652d2
Ash Logan <ash@heyquark.com>
2021-08-24 22:08:21 +1000

    util: move hot BitUtil into header for inlining

--- src/util/BitUtil.h
+++ src/util/BitUtil.h
@@ -9,7 +9,12 @@
 class BitUtil {
 
 public:
-    static unsigned int reverse(unsigned number, int bits);
+    static unsigned int reverse(unsigned number, int bits) {
+        unsigned reversed = 0;
+        for(int i = 0; i < bits; i++)
+            reversed |= ((number >> i) & 1) << (bits - 1 - i);
+        return reversed;
+    }
 };
 
 
